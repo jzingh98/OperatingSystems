dd015f7f86a6d398adbeb2e8d1f91f812fd735e9
jzingh98 <jzingh@ucdavis.edu>
2019-10-06 00:16:07 -0700

    Explicitly initialize struct memory

--- sshell.c
+++ sshell.c
@@ -11,54 +11,63 @@
 
 int main(int argc, char *argv[])
 {
+    // User Input
 	char *input = (char*) malloc(MAX_INPUT_LEN*sizeof(char));
 	size_t len = MAX_INPUT_LEN;
+
+	// Data Structures
 	struct command cmd;
 	struct line myLine;
+
+	// Other Variables
 	pid_t pid;
 	int status;
 
-	// Get User Input
-	fprintf(stdout, "sshell$ ");
-	getline(&input, &len, stdin);
-	//TODO: fix, weird way of removing trailing newline
+    // Initialize Input Loop
+    int continueLoop = 1;
 
-	while ((strcmp("exit", input))){
+    // MAIN INPUT LOOP
+	do {
 
+        // Get User Input
+        fprintf(stdout, "sshell$ ");
+        getline(&input, &len, stdin);
         strtok(input, "\n");
-
         cmd = constructCommand(input);
 
-        // TODO (in progress)
-        myLine = constructLine(input);
-
-
-
+        // Start Process
         pid = fork();
         if(pid == 0) {
+            // Child Process
             execvp(cmd.params[0], cmd.params);
             perror("execvp");
             exit(1);
         } if(pid > 0) {
+            // Parent Process
             waitpid(-1, &status, 0);
             fprintf(stderr, "+ completed '%s' [%d]\n", input, WEXITSTATUS(status));
+            // Get User Input
+            fprintf(stdout, "sshell$ ");
+            getline(&input, &len, stdin);
+            //TODO: fix, weird way of removing trailing newline
+            //return EXIT_SUCCESS;
         } else {
             perror("fork");
             exit(1);
         }
 
-        // Get User Input
-        fprintf(stdout, "sshell$ ");
-        getline(&input, &len, stdin);
-        //TODO: fix, weird way of removing trailing newline
-	}
+        continueLoop = 0;
 
 
-    return EXIT_SUCCESS;
+	} while (continueLoop == 1);
 
 
 
 
-    //Edit
 
 }
+
+
+// (strcmp("exit", input))
+
+
