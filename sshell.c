9a73ddb81a34828e5cc32e6b13fa94be7e6e7495
jzingh98 <jzingh@ucdavis.edu>
2019-10-07 12:50:09 -0700

    Pipeline structured

--- sshell.c
+++ sshell.c
@@ -14,6 +14,7 @@
 #define COMMANDDELIMS "|"
 
 void subcommand(char* input, int currCommand);
+void pipeline(char *process1, char *process2);
 
 
 int main(int argc, char *argv[])
@@ -62,49 +63,8 @@ void subcommand (char* input, currCommand) {
     rest = strdup(input);
     token = strtok_r(rest, COMMANDDELIMS, &rest);
 
-    // Peek at the next command without modifying rest
-    restCopy = strdup(rest);
-    nextToken = strtok_r(restCopy, COMMANDDELIMS, &rest);
-
-    // This is the last recursive call
-    if (nextToken == NULL){
-        if (currCommand == 0){
-            cmd = constructCommand(strdup(token), 1, 1);
-        } else {
-            cmd = constructCommand(strdup(token), 0, 1);
-        }
-    // This is not the last recursive call
-    } else {
-        if (currCommand == 0){
-            cmd = constructCommand(strdup(token),1, 0);
-        } else {
-            cmd = constructCommand(strdup(token), 0, 0);
-        }
-    }
-   // TODO: Check if errored out
-
-
-    // Start Process
-    pid = fork();
-    if(pid == 0) {
-        // Child Process
-        execvp(cmd.params[0], cmd.params);
-        perror("execvp");
-        exit(1);
-    } if(pid > 0) {
-        // Parent Process
-        waitpid(-1, &status, 0);
-        fprintf(stderr, "+ completed '%s' [%d]\n", input, WEXITSTATUS(status));
-        // Get User Input
-        fprintf(stdout, "sshell$ ");
-        getline(&input, &len, stdin);
-        //TODO: fix, weird way of removing trailing newline
-        //return EXIT_SUCCESS;
-    } else {
-        perror("fork");
-        exit(1);
-    }
-
+    // Start Recursion
+    pipeline(token, rest);
 
 
 }
@@ -112,28 +72,54 @@ void subcommand (char* input, currCommand) {
 
 
 
+void pipeline(char *process1, char *process2)
+{
+    int fd[2];
+    pipe(fd); /* Create pipe */
+    if (fork() != 0) {
+        /* Parent */
+        // Always waits for the child to finish.
+        waitpid(-1, &status, 0);
 
+        // Base Case: after waiting, checks process 2, null. Return
+        if (strcmp(process2, NULL) == 0){
+            fprintf(stderr, "+ completed '%s' [%d]\n", input, WEXITSTATUS(status));
+            //return EXIT_SUCCESS;
+        } else{
+            // Recursive Case: after waiting, checks process 2. Not null. Calls itself with token and chop
+            close(fd[0]); /* Don't need read access to pipe */
+            dup2(fd[1], STDOUT_FILENO); /* Replace stdout with the pipe */
+            close(fd[1]); /* Close now unused file descriptor */
 
+            // Chop off first command from input line
+            char* rest = strdup(process2);
+            char* token = strtok_r(rest, COMMANDDELIMS, &rest);
 
-//void pipeline(char *process1, char *process2)
-//{
-//    int fd[2];
-//    pipe(fd); /* Create pipe */
-//    if (fork() != 0) {
-//        /* Parent */
-//        close(fd[0]); /* Don't need read access to pipe */
-//        dup2(fd[1], STDOUT_FILENO); /* Replace stdout with the pipe */
-//        close(fd[1]); /* Close now unused file descriptor */
-//        exec(process1);
-//    } else {
-//        /* Child */
-//        close(fd[1]); /* Don't need write access to pipe */
-//        close(STDIN_FILENO); /* Close existing stdin */
-//        dup(fd[0]); /* And replace it with the pipe */
-//        close(fd[0]); /* Close now unused file descriptor */
-//        exec(process2);
-//    }
-//}
+            // Recursive call
+            pipeline(token, rest);
+        }
+
+    } else {
+        /* Child */
+        // Always executes process 1.
+
+        // Construct command
+        struct command cmd;
+        cmd = constructCommand(strdup(process1), 0, 0);
+
+        // Make Pipe Connection
+        close(fd[1]); /* Don't need write access to pipe */
+        close(STDIN_FILENO); /* Close existing stdin */
+        dup(fd[0]); /* And replace it with the pipe */
+        close(fd[0]); /* Close now unused file descriptor */
+
+        // Execute process
+        execvp(cmd.params[0], cmd.params);
+        perror("execvp");
+        exit(1);
+
+    }
+}
 
 
 
