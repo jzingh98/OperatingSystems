6af67491cfc660cbe84a79ce3396950146224ccd
Tycho Yacub <tychoyacub@gmail.com>
2019-10-08 12:13:59 -0700

    Added simple shell func for testing commands

--- sshell.c
+++ sshell.c
@@ -10,73 +10,55 @@
 #define ERROR_T "1"
 #define ERROR_F "0"
 
+void simpleShell();
+
 int main(int argc, char *argv[])
 {
-    // User Input
-	char *input = (char*) malloc(MAX_INPUT_LEN*sizeof(char));
-	size_t len = MAX_INPUT_LEN;
-
-	// Data Structures
-	struct command cmd;
-	struct line myLine;
-
-	// Other Variables
-	pid_t pid;
-	int status;
-
-    // Initialize Input Loop
-    int continueLoop = 1;
-
-    // MAIN INPUT LOOP
-	do {
-
-        // Get User Input
-        fprintf(stdout, "sshell$ ");
-        getline(&input, &len, stdin);
-        strtok(input, "\n");
-        //cmd = constructCommand(input);
-        myLine = constructLine(input);
-        //check if line construction errored out
-        if(myLine.errored == 0) {
-            fprintf(stderr, "+ completed '%s' [%d]\n", input, 1);
-            continue;
-        }
-        cmd = myLine.commandStructures[0]; // Does not yet work for index > 0
-
-        // Start Process
-        pid = fork();
-        if(pid == 0) {
-            // Child Process
-            execvp(cmd.params[0], cmd.params);
-            perror("execvp");
-            exit(1);
-        } if(pid > 0) {
-            // Parent Process
-            waitpid(-1, &status, 0);
-            fprintf(stderr, "+ completed '%s' [%d]\n", input, WEXITSTATUS(status));
-            // Get User Input
-            fprintf(stdout, "sshell$ ");
-            getline(&input, &len, stdin);
-            //TODO: fix, weird way of removing trailing newline
-            //return EXIT_SUCCESS;
-        } else {
-            perror("fork");
-            exit(1);
-        }
-
-        // TODO: STR Compare
-        continueLoop = 0;
-
-
-	} while (continueLoop == 1);
-
-
-
-
-
+    simpleShell();
+    return EXIT_SUCCESS;
 }
 
+void simpleShell() {
+    // User Input
+    char *input = (char*) malloc(MAX_INPUT_LEN*sizeof(char));
+    size_t len = MAX_INPUT_LEN;
 
-// (strcmp("exit", input))
+    // Data Structures
+    struct command cmd;
 
+    // Other Variables
+    pid_t pid;
+    int status;
+
+    // Get User Input
+    fprintf(stdout, "sshell$ ");
+    getline(&input, &len, stdin);
+    strtok(input, "\n");
+
+    cmd = constructCommand(input, 0, 1);
+
+    //check for error and finish current loop if errored
+    int parsingError = cmd.errored;
+    if(parsingError == 1) {
+        fprintf(stderr, "+ completed '%s' [1]\n", input);
+        simpleShell();
+    }
+
+    // Start Process
+    pid = fork();
+    if(pid == 0) {
+        // Child Process
+        execvp(cmd.params[0], cmd.params);
+        perror("execvp");
+        exit(1);
+    } if(pid > 0) {
+        // Parent Process
+        waitpid(-1, &status, 0);
+        fprintf(stderr, "+ completed '%s' [%d]\n", input, cmd.errored);
+        simpleShell();
+    } else {
+        perror("fork");
+        simpleShell();
+    }
+}
 
